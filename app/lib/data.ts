5d463bc4375ad9fd8d8624e09fdd4ff331fbad13
Qubit <kevinmaxime.r@gmail.com>
2024-10-06 00:17:38 +0200

    Finish tutorial

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -49,6 +49,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
